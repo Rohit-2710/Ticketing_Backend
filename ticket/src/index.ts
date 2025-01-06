e46c9aa2d4c50730189ae62c006efd56a5a7044a
Rohit-2710 <theenigmacode2710@gmail.com>
2025-01-06 23:00:26 +0530

    Feat:-Added tickets microservice and k8s configs

---
+++ ticket/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@tickets2710/common";
+
+import { Request, Response, NextFunction } from "express";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  try {
+    await mongoose.connect("mongodb://ticket-mongo-srv:27017/ticket");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3001, () => {
+    console.log("Ticket Service listening on port 3001!");
+  });
+};
+start();
