aa4e56fb8df4902ca8823803ba026fbcbce3271d
Rohit-2710 <theenigmacode2710@gmail.com>
2025-01-11 23:48:39 +0530

    Feat:-Created first test

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "testkey";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+}, 100 * 1000);
+
+beforeEach(async () => {
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
