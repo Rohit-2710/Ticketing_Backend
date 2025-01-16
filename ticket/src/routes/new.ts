51e2123f89bc63f4e0fb6d397ddbd341b01e3432
Rohit-2710 <theenigmacode2710@gmail.com>
2025-01-16 08:51:04 +0530

    Added NATS streaming server in Ticketing service

--- ticket/src/routes/new.ts
+++ ticket/src/routes/new.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router, NextFunction } from "express";
 import { currentUser, requireAuth, validateRequest } from "@tickets2710/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/tickets";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = Router();
 
@@ -20,6 +22,12 @@ router.post(
     const currentUserId = req.currentUser!.id;
     const ticket = Ticket.build({ title, price, userId: currentUserId });
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );
